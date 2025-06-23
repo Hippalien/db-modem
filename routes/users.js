31b58b0f548ff8a4a3a9ca488fceb95f014cfba7
Elodie Skuta <elodieskt@MacBook-Air-de-Elodie.local>
2025-06-23 16:55:48 +0200

    add environment configuration and user routes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 import db from '../db.js';
 
 async function routes (fastify, options) {
-  fastify.get('/users', async (request, reply) => {
+  fastify.get('/', async (request, reply) => {
     try {
       const users = await db('users');
       return users;
@@ -10,6 +10,22 @@ async function routes (fastify, options) {
       throw err;
     }
   });
+
+
+  fastify.post('/', async (request, reply) => {
+    try {
+      const { name, email, password } = request.body;
+      if (!name || !email || !password){
+        return reply.send({ error: 'Tous les champs sont requis'});
+      }
+      const [user] = await db('users').insert({ name, email, password}).returning('id')
+      return reply.send({ id: user.id });
+    } catch (err) {
+      console.error('Erreur POST /users', err);
+      return reply.send({error: 'Erreur'});
+    }
+  });
+
 }
 
 export default routes;
