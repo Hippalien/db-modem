434d6d85a78539e1d9a640c39f611f7e3421f514
Elodie Skuta <elodieskt@MacBook-Air-de-Elodie.local>
2025-07-09 22:02:08 +0200

    fastify.patch for categories

--- routes/categories.js
+++ routes/categories.js
@@ -1,14 +1,59 @@
 import db from '../db.js';
 
 async function routes (fastify, options) {
-fastify.get('/', async (request, reply) => {
+
+  fastify.get('/', async (request, reply) => {
     try {
       const categories = await db('categories');
       return categories;
     } catch (err) {
-      console.error('Erreur GET /');
+      console.error('Erreur GET /', err);
+      return reply.status(500).send({ error: 'Erreur serveur' });
     }
-});
+  });
+
+  fastify.post('/', async (request, reply) => {
+    try {
+      const { name } = request.body;
+
+      if (!name || typeof name !== 'string') {
+        return reply
+          .status(400)
+          .send({ error: 'Le nom de la catégorie est requis et doit être une chaîne de caractères' });
+      }
+
+      const [id] = await db('categories').insert({ name }).returning('id');
+      return reply.status(201).send({ id });
+    } catch (err) {
+      console.error('Erreur POST /', err);
+      return reply.status(500).send({ error: 'Erreur lors de la création de la catégorie' });
+    }
+  });
+
+  fastify.patch('/:id', async (request, reply) => {
+    try {
+      const { id } = request.params;
+      const { name } = request.body;
+
+      if (!name || typeof name !== 'string') {
+        return reply
+          .status(400)
+          .send({ error: 'Le nom de la catégorie est requis et doit être une chaîne de caractères' });
+      }
+
+      const updatedRows = await db('categories').where({ id }).update({ name });
+
+      if (updatedRows === 0) {
+        return reply.status(404).send({ error: 'Catégorie non trouvée' });
+      }
+
+      return reply.status(200).send({ message: 'Catégorie mise à jour avec succès' });
+    } catch (err) {
+      console.error('Erreur PATCH /:id', err);
+      return reply.status(500).send({ error: 'Erreur lors de la mise à jour de la catégorie' });
+    }
+  });
+
 }
 
 export default routes;
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const fastify = Fastify({
 
 await fastify.register(cors, {
   origin: ['http://localhost:3000', 'http://127.0.1:3000', 'http://localhost:5173'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'], 
 })
 
 fastify.get('/', async (request, reply) => {
